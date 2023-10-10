7c34c5797a2be6e9d3acadb88762fc8e7733f9dc
karthick <prog@ecomsense.in>
2023-10-10 10:01:35 +0530

    get expiry dates from github

--- zero_dte/main.py
+++ zero_dte/main.py
@@ -4,28 +4,33 @@ import inspect
 from time import sleep
 from quotes import option_chain
 from chain import get_ltp_fm_chain
-from netools import load_ymls_from_github, load_dict_from_github
+from netools import load_dict_from_github
+from omspy_brokers.bypass import Bypass
 from toolkit.redis_client import RedisClient
 from toolkit.logger import Logger
 
 logging = Logger(10)
 
 try:
-    kite = RedisClient()
-    kite.authenticate()
-    yaml_file = "Curr_Week_NIFTY_fm_FUT.yaml"
-    if yaml_file is not None:
+    datafeed = RedisClient()
+    if not datafeed.authenticate():
+        SystemExit(1)
+
+    yaml_file = "Curr_Week_NIFTY_fm_FUT"
+    if yaml_file:
         data = load_dict_from_github("netools", "oc-trade", yaml_file)
-        logging.debug(data)
-        if data is not None:
-            d_bld = data
-            bldr = Builder(d_bld)
-            # get ltp of the underlying:Warning to get the ATM
-            ulying = kite.ltp(d_bld['base_script'])
-            base_ltp = ulying[d_bld['base_script']]["last_price"]
-            # more settings for builder
-            atm = bldr.get_atm_strike(base_ltp)
-            d_bld['exchsym'] = bldr.get_syms_fm_atm(atm)
-            d_bld["oc"] = bldr
+        logging.debug(f"github {data=}")
 except Exception as e:
     logging.debug(e)
+
+
+if data:
+    d_bld = data
+    bldr = Builder(d_bld)
+    # get ltp of the underlying:Warning to get the ATM
+    ulying = kite.ltp(d_bld['base_script'])
+    base_ltp = ulying[d_bld['base_script']]["last_price"]
+    # more settings for builder
+    atm = bldr.get_atm_strike(base_ltp)
+    d_bld['exchsym'] = bldr.get_syms_fm_atm(atm)
+    d_bld["oc"] = bldr
--- zero_dte/netools.py
+++ zero_dte/netools.py
@@ -31,14 +31,14 @@ def load_ymls_from_github(repo: str, folder: str):
 def load_dict_from_github(repo: str, folder: str, yml_name: str):
     url = f"https://api.github.com/repos/pannet1/{repo}/contents/{folder}"
     response = requests.get(url)
-    data = None
     if response.status_code == 200:
         files = response.json()
         for file in files:
             if file["type"] == "file" and file["name"] == yml_name + ".yaml":
                 url = file["download_url"]
                 response = requests.get(url)
+                print(response)
                 if response.status_code == 200:
                     data = yaml.load(response.text, Loader=yaml.FullLoader)
                     print(data)
-    return data
+        return data
