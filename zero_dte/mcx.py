c8e2a451e6ffe4b8cd5077f6d86f862246a7e2f8
karthick <prog@ecomsense.in>
2023-10-17 23:15:12 +0530

    fix cls.brkr

--- zero_dte/mcx.py
+++ zero_dte/mcx.py
@@ -7,45 +7,61 @@ class Mcx:
 
     sorting = True
     brkr = None
+    mty = pd.DataFrame()
 
     @ classmethod
     def get_mcx_positions(cls):
-        df_pos = pd.DataFrame(cls.brkr.positions)
-        if len(df_pos) > 0:
-            df_pos = df_pos[['symbol', 'exchange', 'prd', 'token', 'ti',
-                            'quantity', 'urmtom', 'rpnl', 'last_price']]
-            df_pos = df_pos[df_pos['exchange'] == 'MCX']
-            df_pos = df_pos[~df_pos['symbol'].isin(
-                smcx['IGNORE'])]
-            df_print = df_pos.drop(
-                ['exchange', 'prd', 'token', 'ti'], axis=1).set_index('symbol')
-            print("positions \n", df_print)
-        return df_pos
+        try:
+            df_pos = pd.DataFrame(cls.brkr.positions)
+            if len(df_pos) > 0:
+                df_pos = df_pos[['symbol', 'exchange', 'prd', 'token', 'ti',
+                                'quantity', 'urmtom', 'rpnl', 'last_price']]
+                df_pos = df_pos[df_pos['exchange'] == 'MCX']
+                df_pos = df_pos[~df_pos['symbol'].isin(
+                    smcx['IGNORE'])]
+                df_print = df_pos.drop(
+                    ['exchange', 'prd', 'token', 'ti'], axis=1).set_index('symbol')
+                print("positions \n", df_print)
+            return df_pos
+        except Exception as e:
+            logging.warning(e)
+            return cls.mty
 
     @ classmethod
     def get_mcx_m2m(cls):
-        utls.slp_til_nxt_sec()
-        df_pos = cls.get_mcx_positions()
-        unrl = sum(df_pos['urmtom'].values)
-        real = sum(df_pos['rpnl'].values)
-        totl = int(unrl + real)
-        print(f"total: {totl}")
-        return totl
+        try:
+            utls.slp_for(1)
+            df_pos = cls.get_mcx_positions()
+            if len(df_pos) > 0:
+                unrl = sum(df_pos['urmtom'].values)
+                real = sum(df_pos['rpnl'].values)
+                totl = int(unrl + real)
+                print(f"total: {totl}")
+                return totl
+            else:
+                return smcx['STOP']
+        except Exception as e:
+            logging.warning(e)
+            return smcx['STOP']
 
     @ classmethod
     def get_mcx_orders(cls):
-        utls.slp_til_nxt_sec()
-        df_ord = pd.DataFrame(brkr.orders)
-        if len(df_ord) > 0:
-            cols = ['order_id', 'exchange', 'symbol', 'quantity', 'side',
-                    'validity', 'token', 'prd', 'status', ]
-            df_ord = df_ord[cols]
-            df_ord = df_ord[df_ord['exchange'] == 'MCX']
-            condtn = ('OPEN' or 'TRIGGER_PENDING')
-            df_ord = df_ord[df_ord['status'] == condtn]
-            df_ord = df_ord[~df_ord['symbol'].isin(smcx['IGNORE'])]
-            print("orders \n", "df_ord")
-        return df_ord
+        try:
+            utls.slp_for(1)
+            df_ord = pd.DataFrame(cls.brkr.orders)
+            if len(df_ord) > 0:
+                cols = ['order_id', 'exchange', 'symbol', 'quantity', 'side',
+                        'validity', 'token', 'prd', 'status', ]
+                df_ord = df_ord[cols]
+                df_ord = df_ord[df_ord['exchange'] == 'MCX']
+                condtn = ('OPEN' or 'TRIGGER_PENDING')
+                df_ord = df_ord[df_ord['status'] == condtn]
+                df_ord = df_ord[~df_ord['symbol'].isin(smcx['IGNORE'])]
+                print("orders \n", "df_ord")
+            return df_ord
+        except Exception as e:
+            logging.warning(e)
+            return cls.mty
 
     @ staticmethod
     def cancel_order(row):
@@ -53,38 +69,45 @@ class Mcx:
             f"canceling order# {row['order_id']} "
             f"with {row['side']} {row['symbol']}"
         )
-        brkr.order_cancel(row['order_id'])
+        cls.brkr.order_cancel(row['order_id'])
 
     @staticmethod
     def close_positions(row):
-        dir = 1 if row['quantity'] < 0 else -1
-        args = dict(
-            side='B' if row['quantity'] < 0 else 'S',
-            product=row['prd'],
-            exchange=row['exchange'],
-            quantity=abs(row['quantity']),
-            disclosed_quantity=abs(row['quantity']),
-            order_type="LMT",
-            symbol=row['symbol'],
-            price=adjust_ltp(row['last_price'],
-                             smcx['BUFF_PERC'], row['ti'], dir),
-            tag="zero_dte"
-        )
-        logging.debug(f"closing position {args}")
-        resp = brkr.order_place(**args)
-        if resp:
-            logging.info(resp)
+        try:
+            dir = 1 if row['quantity'] < 0 else -1
+            args = dict(
+                side='B' if row['quantity'] < 0 else 'S',
+                product=row['prd'],
+                exchange=row['exchange'],
+                quantity=abs(row['quantity']),
+                disclosed_quantity=abs(row['quantity']),
+                order_type="LMT",
+                symbol=row['symbol'],
+                price=adjust_ltp(row['last_price'],
+                                 smcx['BUFF_PERC'], row['ti'], dir),
+                tag="zero_dte"
+            )
+            logging.debug(f"closing position {args}")
+            resp = cls.brkr.order_place(**args)
+            if resp:
+                logging.debug(resp)
+        except Exception as e:
+            logging.warning(f"while closing positions {e}")
 
     @ classmethod
     def pack_and_go(cls):
-        df_ord = cls.get_mcx_orders()
-        if len(df_ord) > 0:
-            first_row = df_ord.iloc[0]
-            cls.cancel_order(first_row)
-        df_pos = cls.get_mcx_positions()
-        df_pos = df_pos[df_pos['quantity'] != 0]
-        if len(df_pos) > 0:
-            df_pos.sort_values(by='rpnl', ascending=cls.sorting, inplace=True)
-            cls.sorting = False if cls.sorting else True
-            first_row = df_pos.iloc[0]
-            cls.close_positions(first_row)
+        try:
+            df_ord = cls.get_mcx_orders()
+            if len(df_ord) > 0:
+                first_row = df_ord.iloc[0]
+                cls.cancel_order(first_row)
+            df_pos = cls.get_mcx_positions()
+            df_pos = df_pos[df_pos['quantity'] != 0]
+            if len(df_pos) > 0:
+                df_pos.sort_values(
+                    by='rpnl', ascending=cls.sorting, inplace=True)
+                cls.sorting = False if cls.sorting else True
+                first_row = df_pos.iloc[0]
+                cls.close_positions(first_row)
+        except Exception as e:
+            logging.warning(f"{e} while pack and go")
