20a0322996f55830facaeb23e8e36b0459f44f9d
karthick <prog@ecomsense.in>
2023-10-19 21:07:41 +0530

    progagate exception to main loop

--- zero_dte/main.py
+++ zero_dte/main.py
@@ -2,20 +2,29 @@ from mcx import Mcx
 from constants import brkr, smcx, logging
 import pendulum as pdlm
 
-if not brkr.authenticate():
-    logging.error("failed to authenticate")
-    SystemExit(1)
 
-Mcx.brkr = brkr
-squareoff = pdlm.parse(smcx['SQUAREOFF'], fmt="HH:mm").time()
+def authenticate_and_initialize():
+    if not brkr.authenticate():
+        logging.error("Failed to authenticate")
+        raise SystemExit(1)
+
+    Mcx.brkr = brkr
+    squareoff = pdlm.parse(smcx['SQUAREOFF'], fmt="HH:mm").time()
+    return squareoff
+
+
+squareoff = authenticate_and_initialize()
 
 while True:
-    if (
-        (pdlm.now().time() > squareoff)
-        or (Mcx.get_mcx_m2m() < smcx['STOP'])
-    ):
-        Mcx.pack_and_go()
-    else:
-        print(
-            f"time: {pdlm.now().format('HH:mm:ss')} squareoff: {squareoff} ")
+    try:
+        if (pdlm.now().time() > squareoff) or (Mcx.get_mcx_m2m() < smcx['STOP']):
+            Mcx.pack_and_go()
+        else:
+            print(
+                f"time: {pdlm.now().format('HH:mm:ss')} squareoff: {squareoff} ")
+    except Exception as e:
+        # Handle the exception at the main loop level, e.g., log the error
+        logging.error(f"An exception occurred in the main loop: {e}")
 
+        # Re-authenticate and reinitialize
+        squareoff = authenticate_and_initialize()
--- zero_dte/mcx.py
+++ zero_dte/mcx.py
@@ -12,8 +12,12 @@ class Mcx:
     @ classmethod
     def get_mcx_positions(cls):
         try:
+            utls.slp_for(1)
             df_pos = pd.DataFrame(cls.brkr.positions)
-            if len(df_pos) > 0:
+
+            if not df_pos or len(df_pos) == 0:
+                return cls.mty
+            else:
                 df_pos = df_pos[['symbol', 'exchange', 'prd', 'token', 'ti',
                                 'quantity', 'urmtom', 'rpnl', 'last_price']]
                 df_pos = df_pos[df_pos['exchange'] == 'MCX']
@@ -21,35 +25,32 @@ class Mcx:
                     smcx['IGNORE'])]
                 df_print = df_pos.drop(
                     ['exchange', 'prd', 'token', 'ti'], axis=1).set_index('symbol')
-                print("positions \n", df_print)
-            return df_pos
+                print("===       POSITIONS         === \n", df_print)
+                return df_pos
         except Exception as e:
-            logging.warning(e)
-            return cls.mty
+            raise
 
     @ classmethod
     def get_mcx_m2m(cls):
-        try:
-            utls.slp_for(1)
-            df_pos = cls.get_mcx_positions()
-            if len(df_pos) > 0:
-                unrl = sum(df_pos['urmtom'].values)
-                real = sum(df_pos['rpnl'].values)
-                totl = int(unrl + real)
-                print(f"total: {totl}")
-                return totl
-            else:
-                return smcx['STOP']
-        except Exception as e:
-            logging.warning(e)
-            return smcx['STOP']
+        df_pos = cls.get_mcx_positions()
+        if len(df_pos) > 0:
+            unrl = sum(df_pos['urmtom'].values)
+            real = sum(df_pos['rpnl'].values)
+            totl = int(unrl + real)
+            print(f"total: {totl}")
+            return totl
+        else:
+            print(f"no positions to STOP: {smcx['STOP']}")
+            return 0
 
     @ classmethod
     def get_mcx_orders(cls):
         try:
             utls.slp_for(1)
             df_ord = pd.DataFrame(cls.brkr.orders)
-            if len(df_ord) > 0:
+            if not df_ord or len(df_ord) == 0:
+                return cls.mty
+            else:
                 cols = ['order_id', 'exchange', 'symbol', 'quantity', 'side',
                         'validity', 'token', 'prd', 'status', ]
                 df_ord = df_ord[cols]
@@ -57,23 +58,29 @@ class Mcx:
                 condtn = ('OPEN' or 'TRIGGER_PENDING')
                 df_ord = df_ord[df_ord['status'] == condtn]
                 df_ord = df_ord[~df_ord['symbol'].isin(smcx['IGNORE'])]
-                print("orders \n", "df_ord")
-            return df_ord
+                print("===          POSITIONS           ===\n", "df_ord")
+                return df_ord
         except Exception as e:
-            logging.warning(e)
+            logging.warning(f"{e} while getting ORDERS")
             return cls.mty
 
-    @ staticmethod
+    @staticmethod
     def cancel_order(row):
-        logging.debug(
-            f"canceling order# {row['order_id']} "
-            f"with {row['side']} {row['symbol']}"
-        )
-        cls.brkr.order_cancel(row['order_id'])
+        try:
+            utls.slp_for(1)
+            cls.brkr.order_cancel(row['order_id'])
+        except Exception as e:
+            logging.warning(f"{e} while CANCELLING order for {row['symbol']}")
+        else:
+            logging.debug(
+                f"cancelled order# {row['order_id']} "
+                f"with {row['side']} {row['symbol']} "
+            )
 
     @staticmethod
     def close_positions(row):
         try:
+            utls.slp_for(0.5)
             dir = 1 if row['quantity'] < 0 else -1
             args = dict(
                 side='B' if row['quantity'] < 0 else 'S',
@@ -89,10 +96,14 @@ class Mcx:
             )
             logging.debug(f"closing position {args}")
             resp = cls.brkr.order_place(**args)
-            if resp:
-                logging.debug(resp)
         except Exception as e:
-            logging.warning(f"while closing positions {e}")
+            logging.error(f"{e} while placing order")
+
+        if resp:
+            print(resp)
+        else:
+            logging.warning(
+                f"no reponse while trying to close {row['symbol']}")
 
     @ classmethod
     def pack_and_go(cls):
@@ -110,4 +121,4 @@ class Mcx:
                 first_row = df_pos.iloc[0]
                 cls.close_positions(first_row)
         except Exception as e:
-            logging.warning(f"{e} while pack and go")
+            raise (e)
